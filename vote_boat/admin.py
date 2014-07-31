2cf657253e4a6ed80921cacd56ca664870a70b74
Brian Kimmig <bkimmig@comcast.net>
2014-07-31 14:55:39 -0600

    added the class Idea to models.py and registered it in admin.py

--- vote_boat/admin.py
+++ vote_boat/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 from django.contrib import admin
-from .models import Poll
+from .models import Poll,Idea
+
 
 admin.site.register(Poll)
-
+admin.site.register(Idea)
--- vote_boat/models.py
+++ vote_boat/models.py
@@ -5,9 +5,17 @@ from django.utils import timezone
 class Poll (models.Model):
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)    
-    title = models.CharField(max_length=45)
+    title = models.CharField(max_length=100)
     description = models.CharField(max_length=300,default="")
     #    admin_user_id 
 
     def __unicode__ (self):
         return u"Poll : {}".format(self.title)
+
+class Idea (models.Model):
+    poll_id = models.ForeignKey(Poll)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=300,default="")
+    
+    def __unicode__ (self):
+        return u"Idea : {}".format(self.title)   
