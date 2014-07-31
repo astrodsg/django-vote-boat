a3262fd7b526236f20ceeaf61e95bf7ce202eeb6
astrodsg_laptop <gregersen.dylan@gmail.com>
2014-07-31 14:34:27 -0600

    added model and admin

--- vote_boat/models.py
+++ vote_boat/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
-# Create your models here.
+class Poll (models.Model):
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)    
+    title = models.CharField(max_length=45)
+    description = models.CharField(max_length=300,default="")
+    #    admin_user_id 
+
+    def __unicode__ (self):
+        return u"Poll : {}".format(self.title)
